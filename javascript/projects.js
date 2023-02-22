01b54dec917f01af1ad19617cd6df7ae6d77a68c
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2023-02-21 19:02:34 -0500

    DId a lot of the projects page
    Finsished the projects page exept adding the projects. But the CSS, JS and stuff for that I did

---
+++ javascript/projects.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddentElements = document.querySelectorAll('.hidden')
+hiddentElements.forEach((el) => observer.observe(el))
