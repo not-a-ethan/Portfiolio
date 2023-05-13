9ed94dda2b68a06940ee438fa876e4546c6ad8ca
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2023-05-13 14:23:38 -0400

    Fixed the nav bar work on mobile
    The drop down for projects on the nav bar now works on mobile. Need to fix the formatting though

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,8 +1,27 @@
 const projectsListItem = document.getElementById("projectsLink");
+const projectListDropDown = document.getElementById("projectListDropDown");
 const windowWidth = window.innerWidth;
 
 if (windowWidth <= 600) {
+  projectsListItem.addEventListener("click", onMobileClick);
   projectsListItem.innerHTML = '<p>PROJECTS &#9660</p>';
 } else if (windowWidth >= 601) {
   projectsListItem.innerHTML = '<a href="https://not-a-ethan.github.io/projects">PROJECTS &#9660 and some other stuff</a>';
+}
+
+function onMobileClick() {
+  const stylesOfTheElement = window.getComputedStyle(projectListDropDown)
+  console.log("clicked on mobile")
+  console.log(stylesOfTheElement.getPropertyValue("opacity"))
+  if (stylesOfTheElement.getPropertyValue("opacity") == "0") {
+    console.log("opacity was 0")
+    projectListDropDown.style.opacity = "1";
+    projectListDropDown.style.visibility = "visible";
+    projectListDropDown.style.transition = "all 0.5s ease-in";
+  } else {
+    console.log("opacity was 1")
+    projectListDropDown.style.opacity = "0";
+    projectListDropDown.style.visibility = "hidden";
+    projectListDropDown.style.transition = "all 0.5s ease-in-out";
+  }
 }
