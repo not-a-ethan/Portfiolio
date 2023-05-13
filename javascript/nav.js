dd97fb03e4e165d725a6530d2a5183f5ff389f79
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2023-05-13 15:46:22 -0400

    Update nav.js

--- javascript/nav.js
+++ javascript/nav.js
@@ -5,10 +5,10 @@ const windowWidth = window.innerWidth;
 if (windowWidth <= 600) {
   projectsListItem.addEventListener("click", onMobileClick);
   projectsListItem.innerHTML = '<p>PROJECTS &#9660</p>';
+  projectsListItem.querySelector("p").style.fontSize = "2.5vw"
 } else if (windowWidth >= 601) {
-  projectsListItem.innerHTML = '<p><a href="https://not-a-ethan.github.io/projects">PROJECTS &#9660 and some other stuff</a></p>';
+  projectsListItem.innerHTML = '<p><a href="https://not-a-ethan.github.io/projects">PROJECTS &#9660</a></p>';
 }
-projectsListItem.querySelector("p").style.fontSize = "2.5vw"
 
 function onMobileClick() {
   const stylesOfTheElement = window.getComputedStyle(projectListDropDown)
