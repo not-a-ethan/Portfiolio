4c360aff674c5a0f6aa2841f018ce154a9ee8f2b
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2023-05-13 15:39:47 -0400

    Made the nav bar work on mobile

--- javascript/nav.js
+++ javascript/nav.js
@@ -6,20 +6,17 @@ if (windowWidth <= 600) {
   projectsListItem.addEventListener("click", onMobileClick);
   projectsListItem.innerHTML = '<p>PROJECTS &#9660</p>';
 } else if (windowWidth >= 601) {
-  projectsListItem.innerHTML = '<a href="https://not-a-ethan.github.io/projects">PROJECTS &#9660 and some other stuff</a>';
+  projectsListItem.innerHTML = '<p><a href="https://not-a-ethan.github.io/projects">PROJECTS &#9660 and some other stuff</a></p>';
 }
+projectsListItem.querySelector("p").style.fontSize = "2.5vw"
 
 function onMobileClick() {
   const stylesOfTheElement = window.getComputedStyle(projectListDropDown)
-  console.log("clicked on mobile")
-  console.log(stylesOfTheElement.getPropertyValue("opacity"))
   if (stylesOfTheElement.getPropertyValue("opacity") == "0") {
-    console.log("opacity was 0")
     projectListDropDown.style.opacity = "1";
     projectListDropDown.style.visibility = "visible";
     projectListDropDown.style.transition = "all 0.5s ease-in";
   } else {
-    console.log("opacity was 1")
     projectListDropDown.style.opacity = "0";
     projectListDropDown.style.visibility = "hidden";
     projectListDropDown.style.transition = "all 0.5s ease-in-out";
