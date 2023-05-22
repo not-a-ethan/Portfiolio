ba091858cb3f9697e736a473757544809f7a18aa
Restyled.io <commits@restyled.io>
2023-05-22 01:41:45 +0000

    Restyled by clang-format

--- javascript/aboutMe.js
+++ javascript/aboutMe.js
@@ -1,22 +1,20 @@
-const observerLeft = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('showLeft')
-    } else {
-      entry.target.classList.remove('showLeft')
-    }
-  })
-})
+const observerLeft =
+    new IntersectionObserver((entries) => {entries.forEach((entry) => {
+                               if (entry.isIntersecting) {
+                                 entry.target.classList.add('showLeft')
+                               } else {
+                                 entry.target.classList.remove('showLeft')
+                               }
+                             })})
 
-const observerRight = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('showRight')
-    } else {
-      entry.target.classList.remove('showRight')
-    }
-  })
-})
+const observerRight =
+    new IntersectionObserver((entries) => {entries.forEach((entry) => {
+                               if (entry.isIntersecting) {
+                                 entry.target.classList.add('showRight')
+                               } else {
+                                 entry.target.classList.remove('showRight')
+                               }
+                             })})
 
 const hiddentElementsLeft = document.querySelectorAll('.hiddenLeft')
 hiddentElementsLeft.forEach((el) => observerLeft.observe(el))
