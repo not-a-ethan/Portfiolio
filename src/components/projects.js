b2373681d4d001cafd61f335fb310c8cf4d30590
Restyled.io <commits@restyled.io>
2023-07-06 02:48:18 +0000

    Restyled by prettier

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 function Projects() {
   useEffect(() => {
@@ -22,14 +22,17 @@ function Projects() {
       });
     });
 
-    const hiddentElementsLeftProject =
-        document.querySelectorAll(".project-hiddenLeft");
+    const hiddentElementsLeftProject = document.querySelectorAll(
+      ".project-hiddenLeft"
+    );
     hiddentElementsLeftProject.forEach((el) => observerLeftProject.observe(el));
 
-    const hiddentElementsRightProject =
-        document.querySelectorAll(".project-hiddenRight");
+    const hiddentElementsRightProject = document.querySelectorAll(
+      ".project-hiddenRight"
+    );
     hiddentElementsRightProject.forEach((el) =>
-                                            observerRightProject.observe(el));
+      observerRightProject.observe(el)
+    );
   });
 
   return (
@@ -37,18 +40,34 @@ function Projects() {
       <header id="project-header">
         <h1>Projects</h1>
 
-        <p id="project-p-header-tag">Here I will list my best projects I am most proud of. The list will be in chronilogical order</p>
+        <p id="project-p-header-tag">
+          Here I will list my best projects I am most proud of. The list will be
+          in chronilogical order
+        </p>
         <br />
         <p id="project-p-header-tag">
-          Not all of my projects will be listed here. Only the greatest of them will be.You can view all my projects on my <a href="https://github.com/not-a-ethan?tab=repositories">Github Profile</a>. If you have any idea for what I should make feel free to suggest it by <Link to="/contact">contacting me</Link>.</p>
+          Not all of my projects will be listed here. Only the greatest of them
+          will be.You can view all my projects on my{" "}
+          <a href="https://github.com/not-a-ethan?tab=repositories">
+            Github Profile
+          </a>
+          . If you have any idea for what I should make feel free to suggest it
+          by <Link to="/contact">contacting me</Link>.
+        </p>
       </header>
 
       <main id="project-main">
-        <section className="project-projectLeft project-hiddenRight" id="project-portfolio">
+        <section
+          className="project-projectLeft project-hiddenRight"
+          id="project-portfolio"
+        >
           <h2>My portfolio(this website) </h2>
           <div className="project-imgAndText">
             <div className="project-img">
-              <img src="./images/portfolio.png "alt="Screenshot of the Home page of this website"/>
+              <img
+                src="./images/portfolio.png "
+                alt="Screenshot of the Home page of this website"
+              />
 
               <div className="project-techs">
                 <img src="./images/skills/HTML.png" alt="HTML Logo" />
@@ -56,25 +75,32 @@ function Projects() {
                 <img src="./images/skills/CSS.png" alt="CSS Logo" />
                 <br />
                 <img
-  src = "./images/skills/Javascript.png"
-  alt = "JavaScript Logo" / > </div>
+                  src="./images/skills/Javascript.png"
+                  alt="JavaScript Logo"
+                />{" "}
+              </div>
             </div>
 
-        <div className = "project-textOrGhLink">
-        <p>My protfolio website is this very site you are looking at right
-  now.This site contains my biggest projects and the ones I am
-  most proud of such as Coin Catalog.It also tells you and others
-  how to contact me and a little about myself
-          .</p>
+            <div className="project-textOrGhLink">
+              <p>
+                My protfolio website is this very site you are looking at right
+                now.This site contains my biggest projects and the ones I am
+                most proud of such as Coin Catalog.It also tells you and others
+                how to contact me and a little about myself .
+              </p>
 
-              <a className="project-gh-Button" href="https:/ /
+              <a
+                className="project-gh-Button"
+                href="https:/ /
                         github.com / not -
-                    a - ethan / not - a - ethan.github.io ">
-                < img src = "./images/githubLogo.png" alt = "Github Logo" />
-      <pre>Github</pre>
-              </a></div>
+                    a - ethan / not - a - ethan.github.io "
+              >
+                <img src="./images/githubLogo.png" alt="Github Logo" />
+                <pre>Github</pre>
+              </a>
+            </div>
           </div>
-      </section>
+        </section>
 
         <section
           className="project-projectRight project-hiddenLeft"
@@ -82,31 +108,41 @@ function Projects() {
         >
           <h2>Coin Catalog</h2>
 
-      <div className = "project-imgAndText"><div className = "project-img">< img
-  src = "./images/coinCatalogProject.png"
-  alt = "A screenshot from the home page of Coincatalog" / >
+          <div className="project-imgAndText">
+            <div className="project-img">
+              <img
+                src="./images/coinCatalogProject.png"
+                alt="A screenshot from the home page of Coincatalog"
+              />
 
-        <div className = "project-techs">
-        <img src = "./images/skills/HTML.png" alt = "HTML Logo" /><br />
-        <img src = "./images/skills/CSS.png" alt = "CSS Logo" />
-        </div>
+              <div className="project-techs">
+                <img src="./images/skills/HTML.png" alt="HTML Logo" />
+                <br />
+                <img src="./images/skills/CSS.png" alt="CSS Logo" />
+              </div>
             </div>
 
-        <div className = "project-textOrGhLink">
-        <p>Coin Catalog is an open sourse project I contribute and is the
-  sole maintainer of .It has info about coins from all through out
-  American history and is constantly growing
-          .</p>
+            <div className="project-textOrGhLink">
+              <p>
+                Coin Catalog is an open sourse project I contribute and is the
+                sole maintainer of .It has info about coins from all through out
+                American history and is constantly growing .
+              </p>
 
-              <a className="project-gh-Button" href="https:/ /
+              <a
+                className="project-gh-Button"
+                href="https:/ /
                         github.com / Coin -
-                    Catalog ">
-                < img src = "./images/githubLogo.png" alt = "Github Logo" />
-      <pre>Github</pre>
-              </a></div>
+                    Catalog "
+              >
+                <img src="./images/githubLogo.png" alt="Github Logo" />
+                <pre>Github</pre>
+              </a>
+            </div>
           </div>
-      </section>
-      </main>< />
+        </section>
+      </main>
+    </>
   );
 }
 
