ff53b1357d669a899297aca74850f46dc7ff2d83
Restyled.io <commits@restyled.io>
2023-07-06 01:35:38 +0000

    Restyled by clang-format

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,34 +19,30 @@ function Contact() {
         <ul>
           <li className="contact-li">
             <img
-              src="./images/discordLogo.svg"
-              alt="Discord logo"
-              className="contact-image"
-            />{" "}
-            Discord: <code className="contact-code">not_ethan.</code>
+  src = "./images/discordLogo.svg"
+  alt = "Discord logo"
+  className =
+      "contact-image" / >
+      {" "} Discord:
+          <code className = "contact-code">not_ethan.</code>
           </li>
-          <li className="contact-li">
-            <img
-              src="./images/githubLogo.png"
-              alt="Github Logo"
-              className="contact-image"
-            />{" "}
-            <a href="https://github.com/not-a-ethan">Github Profile</a>
-          </li>
-          <li className="contact-li">
-            <img
-              src="./images/replitLogo.png"
-              alt="Replit Logo"
-              className="contact-image"
-            />{" "}
-            <a href="https://ask.replit.com/u/not-ethan/summary">
-              Replit Ask Profile
-            </a>
-          </li>
-          {/* <img src="./images/projectNuLogo.bmp" alt="Project Nu Logo" /> <a href="https://projectnu.org/u/ethan/summary">Project Nu Profile</a></li> */}
-        </ul>
-      </main>
-    </>
+      <li className = "contact-li">< img
+  src = "./images/githubLogo.png"
+  alt = "Github Logo"
+  className = "contact-image" / >
+              {" "}<a href = "https://github.com/not-a-ethan">Github
+                  Profile</a>
+          </li><li className = "contact-li">< img
+  src = "./images/replitLogo.png"
+  alt = "Replit Logo"
+  className = "contact-image" / > {" "}<
+                                      a href =
+                                          "https://ask.replit.com/u/not-ethan/summary">
+                                      Replit Ask Profile</a>
+          </li>{/* <img src="./images/projectNuLogo.bmp" alt="Project Nu Logo" /> <a href="https://projectnu.org/u/ethan/summary">Project Nu Profile</a></li> */}<
+                                          /ul>
+      </main><
+              />
   );
 }
 
