7042087fbebe42904ece0b066d9773d480c90d71
Restyled.io <commits@restyled.io>
2023-07-06 01:34:01 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
-import "./CSS/globle.css"
-import "./CSS/nav.css"
-import "./CSS/home.css"
-import "./CSS/aboutMe.css"
-import "./CSS/projects.css"
-import "./CSS/contact.css"
+import "./CSS/globle.css";
+import "./CSS/nav.css";
+import "./CSS/home.css";
+import "./CSS/aboutMe.css";
+import "./CSS/projects.css";
+import "./CSS/contact.css";
 
-import React from 'react';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import About from './components/about.js';
-import Contact from './components/contact.js';
-import Home from './components/home.js';
-import Nav from './components/nav.js';
-import Projects from './components/projects.js';
+import About from "./components/about.js";
+import Contact from "./components/contact.js";
+import Home from "./components/home.js";
+import Nav from "./components/nav.js";
+import Projects from "./components/projects.js";
 
 const router = createBrowserRouter([
   {
-  path: "/",
-      element: (
-          <div><Nav /><Home />
-          </div>
-    )
+    path: "/",
+    element: (
+      <div>
+        <Nav />
+        <Home />
+      </div>
+    ),
   },
   {
     path: "about-me",
     element: (
       <div>
         <Nav />
-          <About />
-          </div>
-    )
+        <About />
+      </div>
+    ),
   },
   {
     path: "projects",
     element: (
       <div>
         <Nav />
-          <Projects />
-          </div>
-    )
+        <Projects />
+      </div>
+    ),
   },
   {
     path: "contact",
     element: (
       <div>
         <Nav />
-          <Contact />
-          </div>
-    )
-  }
-])
+        <Contact />
+      </div>
+    ),
+  },
+]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,13 @@ function Home() {
       <header>
         <h1>Ethan's Portfolio</h1>
 
-        <p>Hey there! I'm Ethan. I love coding espesaly discord bots and the front end of websites. I love helping others even more helping them get over road bumps they face when first learning! I am an Admin on <a href="https://ask.replit.com">Replit Ask</a> and a Moderator on <a href="https://replit.com">Replit</a>.</p>
+        <p>
+          Hey there! I'm Ethan. I love coding espesaly discord bots and the
+          front end of websites. I love helping others even more helping them
+          get over road bumps they face when first learning! I am an Admin on{" "}
+          <a href="https://ask.replit.com">Replit Ask</a> and a Moderator on{" "}
+          <a href="https://replit.com">Replit</a>.
+        </p>
       </header>
 
       <hr />
@@ -39,7 +45,7 @@ function Home() {
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
