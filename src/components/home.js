496be7d1129b17b8999ddd00823ee0767da3fef6
Restyled.io <commits@restyled.io>
2023-07-07 02:17:12 +0000

    Restyled by prettier

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,13 @@ function Home() {
       <header>
         <h1>Ethan's Portfolio</h1>
 
-        <p>Hey there! I'm Ethan. I love coding, especially discord bots and the front end of websites. I love helping others even more helping them get over road bumps they face when first learning! I am an Admin on <a href="https://ask.replit.com">Replit Ask</a> and a Moderator on <a href="https://replit.com">Replit</a>.</p>
+        <p>
+          Hey there! I'm Ethan. I love coding, especially discord bots and the
+          front end of websites. I love helping others even more helping them
+          get over road bumps they face when first learning! I am an Admin on{" "}
+          <a href="https://ask.replit.com">Replit Ask</a> and a Moderator on{" "}
+          <a href="https://replit.com">Replit</a>.
+        </p>
       </header>
 
       <hr />
