55db1b938eb86e28996044bd217868fc77498141
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2023-09-23 13:12:02 -0400

    All but project page
    * Combined the home page and about me page
    * Added the redesign for the now combined page
    * Re did the nav bar for it

---
+++ src/components/home/skill.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Skill(props) {
+  /*
+    props
+    skillImgPath
+    skillImgAlt
+    skillName
+    skillDescription
+  */
+
+  return (
+    <div className="skill">
+      <p style={{display: "inline"}} className='skill-name secondary-text'>{props.skillName} <img alt={props.skillImgAlt} src={props.skillImgPath} className='skill-image' /></p>
+      <p className='skill-description'>{props.skillDescription}</p>
+    </div>
+  )
+}
+
+export default Skill
