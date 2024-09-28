cdd0fdd3325e704ddfba054c536fbd74c0a100f4
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2024-09-28 17:56:44 -0400

    Added support page & some package stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./CSS/nav.css";
 import "./CSS/home.css";
 import "./CSS/projects.css";
 
-import "./CSS/skill.css"
+import "./CSS/skill.css";
 
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -11,30 +11,40 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/home.js";
 import Nav from "./components/nav.js";
 import Projects from "./components/projects.js";
+import Support from "./components/support.js";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <Nav currentPage="home" />
-        <Home />
-      </div>
-    ),
-  },
-  {
-    path: "projects",
-    element: (
-      <div>
-        <Nav currentPage="projects"  />
-        <Projects />
-      </div>
-    ),
-  }
+    {
+        path: "/",
+        element: (
+            <div>
+                <Nav currentPage="home" />
+                <Home />
+            </div>
+        ),
+    },
+    {
+        path: "projects",
+        element: (
+            <div>
+                <Nav currentPage="projects" />
+                <Projects />
+            </div>
+        ),
+    },
+    {
+        path: "/support",
+        element: (
+            <div>
+                <Nav currentPage="support" />
+                <Support />
+            </div>
+        ),
+    },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/support.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+
+function Support() {
+    return (
+        <>
+            <header>
+                <h1>Support me</h1>
+
+                <p>
+                    If you would like to support me please consider using one of
+                    my refferal links
+                </p>
+            </header>
+
+            <main>
+                <ul>
+                    <li>
+                        <a href="https://m.do.co/c/dea049b7c215">
+                            Digital Ocean
+                        </a>
+                    </li>
+                </ul>
+            </main>
+        </>
+    );
+}
+
+export default Support;
