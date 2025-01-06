297d29c7347f068c724dbadcd2d6c5874da56686
Ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-01-05 21:04:03 -0500

    Some basic changes

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
-# My own website
-
 ## Description
 
-This website is to show me and other people what I am able to do with programing and my other projects I have made.
-
-## How to use the site
-
-You can use the site my visiting it [here](https://not-ethan.tech). Just scroll up and down and click on the links.
+This site is my "portfolio". It is to show me and other people what I have done with programing and computers.
 
 ## Technologies
 
-I used React and CSS to build this website. Some projects on the page are made in JS.
-
-## Collaborators
-
-[I (Ethan)](<[https://github.com/ethan-master-coding](https://github.com/not-a-ethan/)>) have worked on the site.
+I used React and CSS to build this website.
--- src/components/home.js
+++ src/components/home.js
@@ -22,63 +22,63 @@ function Home() {
     <main id="home" className="main">
       <h1 className="secondary-text hidden">About Me</h1>
 
-      <p className="hidden">
+      <p className="hidden hidden-left">
         <span className="secondary-text">Hey I'm Ethan</span>. I'm a student that <span className="secondary-text">loves</span> to <span className="secondary-text">Program</span>. I am currently on the <span className="secondary-text">East Coast</span> of the <span className="secondary-text">United states</span>. I <span className="secondary-text">help others</span> with programming <span className="secondary-text">sharing</span> what I have learned over the years with new comers.
       </p>
 
-      <hr className="hidden" />
+      <hr className="hidden hidden-left" />
 
-      <h2 className="secondary-text hidden">My Programming Journey</h2>
+      <h2 className="secondary-text hidden hidden-right">My Programming Journey</h2>
 
-      <p className="hidden">
+      <p className="hidden hidden-left">
         I started in <span className="secondary-text">January 2020</span> to make a <span className="secondary-text">website</span> for something in real life. I started with <span className="secondary-text">JavaScript.</span> Back then I thought "true programmers" used binary and every language can do everything the same. So I learned up to conditionals or so and gave up since lock down happened.
       </p>
 
-      <p className="hidden">
-        Then in 2021 I started again learned <span className="secondary-text">HTML, CSS</span> and <span className="secondary-text">JS</span> (not for webdev) well. So I made some websites and then learned JS for <span className="secondary-text">frontend webdev</span>. 
+      <p className="hidden hidden-right">
+        Then in 2021 I started again learned <span className="secondary-text">HTML, CSS</span> and <span className="secondary-text">JS</span> (JS not for webdev). So I made some websites and then learned JS for <span className="secondary-text">frontend webdev</span>. 
       </p>
 
-      <p className="hidden">
-        Then learned about front end frame works and tried learning <span className="secondary-text">React</span>. Around a year later I actually learned it.
+      <p className="hidden hidden-left">
+        Then learned about front end frame works and tried learning <span className="secondary-text">React</span> but gave up after I finished the basics. Around a year later I re-learned the basics and <span className="secondary-text">taught myself</span> the rest of the framework.
       </p>
 
-      <p className="hidden">
-        Then now a days I learned <span className="secondary-text">SQL</span> and improving my <span className="secondary-text">Next.js</span> skills as part of my journey of learning <span className="secondary-text">backend webdev</span>.
+      <p className="hidden hidden-right">
+        After that in 2024 I learned <span className="secondary-text">SQL</span> and improving my <span className="secondary-text">Next.js</span> skills as part of my journey of learning <span className="secondary-text">backend webdev</span>.
       </p>
 
-      <p className="secondary-text hidden">Check out what I've learned</p>
+      <p className="secondary-text hidden hidden-left">Check out what I've learned</p>
       <p style={{fontSize: 50, margin: 10}} className="hidden" >&#8595;</p>
 
       <div style={{display: "grid"}}>
-        <div className="row-one column-one gridItem hidden">
+        <div className="row-one column-one gridItem hidden hidden-left">
           <Skill skillName="HTML" skillImgPath="./images/skills/HTML.png" skillImgAlt="HTML Logo" skillDescription="HTML is the a languge to build the front end of a webpage." />
         </div>
 
-        <div className="row-one column-two gridItem hidden">
+        <div className="row-one column-two gridItem hidden hidden-left">
           <Skill skillName="CSS" skillImgPath="./images/skills/CSS.png" skillImgAlt="CSS Logo" skillDescription="CSS is used to style HTML to make it look good." />
         </div>
 
-        <div className="row-one column-three gridItem hidden">
+        <div className="row-one column-three gridItem hidden hidden-left">
           <Skill skillName="JavaScript" skillImgPath="./images/skills/Javascript.png" skillImgAlt="JavaScript Logo" skillDescription="Javascript can be used for almost anything. It is used to make HTML interactive, the backend of a website or just about anything." />
         </div>
 
-        <div className="row-two column-one gridItem hidden">
+        <div className="row-two column-one gridItem hidden hidden-right">
           <Skill skillName="TypeScript" skillImgPath="./images/skills/TypeScript.png" skillImgAlt="TypeScript Logo" skillDescription="Javascript but with declared types" />
         </div>
 
-        <div className="row-two column-two gridItem hidden">
-          <Skill skillName="SQL" skillImgPath="./images/skills/SQL.png" skillImgAlt="SQL Logo" skillDescription="SQL is used to interact with databses from reading to dbs to writing to them." />
+        <div className="row-two column-two gridItem hidden hidden-right">
+          <Skill skillName="SQL" skillImgPath="./images/skills/SQL.png" skillImgAlt="SQL Logo" skillDescription="SQL is used to interact with databses. From reading to writing to databases." />
         </div>
 
-        <div className="row-two column-three gridItem hidden">
+        <div className="row-two column-three gridItem hidden hidden-right">
           <Skill skillName="React" skillImgPath="./images/skills/React.png" skillImgAlt="React Logo" skillDescription="React is a Front end web framework and could be used full stack with another framework like Next.js or Remix." />
         </div>
 
-        <div className="row-three column-one gridItem hidden">
+        <div className="row-three column-one gridItem hidden hidden-left">
           <Skill skillName="Github" skillImgPath="./images/skills/Github.png" skillImgAlt="Github Logo" skillDescription="Github is version control in the web that uses Git. It is also used to host code and open source projects." />
         </div>
 
-        <div className="row-three column-two gridItem hidden">
+        <div className="row-three column-two gridItem hidden hidden-left">
           <Skill skillName="Next.js" skillImgPath="./images/skills/Nextjs.png" skillImgAlt="Next.js Logo" skillDescription="Next.js is a full stack web framework that uses React." />
         </div>
       </div> 
