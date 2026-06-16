3e7b7ccf40081a14a8d799c25d19259afba31c27
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2026-06-15 22:20:43 -0400

    Trying to fix depoyment error
    I hate this, works on local machine but not on prod server

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
+  build: {
+    outDir: path.join(__dirname, "_static")
+  }
 });
