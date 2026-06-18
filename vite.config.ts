3d27fa6edba5ab03a9e73730d8dfb8a349ea4eb9
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2026-06-18 13:32:53 -0400

    Updated configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import path, { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
-  },
-  build: {
-    outDir: path.join(__dirname, "_static"),
   },
   base: "/"
 });
